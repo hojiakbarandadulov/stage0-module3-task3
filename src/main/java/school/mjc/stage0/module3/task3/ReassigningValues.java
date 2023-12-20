d7a99867c339c753ff4875832f4486270f35d131
Your Name <you@example.com>
2023-12-21 00:42:46 +0500

    zor judayam

--- src/main/java/school/mjc/stage0/module3/task3/ReassigningValues.java
+++ src/main/java/school/mjc/stage0/module3/task3/ReassigningValues.java
@@ -3,5 +3,24 @@ package school.mjc.stage0.module3.task3;
 public class ReassigningValues {
     public static void main(String[] args) {
         //write code here
+        int linkToFirst=1;
+        int linkToSecond=10;
+        int linkToThird =100;
+        System.out.println(linkToFirst);
+        System.out.println(linkToSecond);
+        System.out.println(linkToThird);
+
+        linkToFirst=15;
+        linkToSecond=6;
+        linkToThird=4;
+        System.out.println(linkToFirst);
+        System.out.println(linkToSecond);
+        System.out.println(linkToThird);
+        linkToFirst=1;
+        linkToSecond=10;
+        linkToThird=100;
+        System.out.println(linkToFirst);
+        System.out.println(linkToSecond);
+        System.out.println(linkToThird);
     }
 }
